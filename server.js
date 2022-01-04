9932477c4770c3db78a3515f933a66095368a1c8
Hritick Jaiswal <newbiehritick@gmail.com>
2022-01-04 21:52:09 +0530

    Connected to Mongodb

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use("/", express.static(path.join(__dirname, "static")));
 app.use(bodyParser.json());
+
+mongoose
+  .connect("mongodb://localhost:27017/jwtLogin", {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("Mongodb connected"));
 
 app.post("/api/register", (req, res) => {
   console.log(req.body);
