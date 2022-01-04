9e0e63df4940d1ea678495b5981bd85dce0cdf3f
Hritick Jaiswal <newbiehritick@gmail.com>
2022-01-04 22:42:47 +0530

    Added password encryption

--- server.js
+++ server.js
@@ -2,20 +2,38 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const User = require("./models/user");
+const bcrypt = require("bcryptjs");
 
 const app = express();
 
+//* Using midllewares
 app.use("/", express.static(path.join(__dirname, "static")));
 app.use(bodyParser.json());
 
+//* Connecting to mongodb
 mongoose
   .connect("mongodb://localhost:27017/jwtLogin", {
     useNewUrlParser: true,
   })
   .then(() => console.log("Mongodb connected"));
 
-app.post("/api/register", (req, res) => {
-  console.log(req.body);
+//* POST route
+app.post("/api/register", async (req, res) => {
+  const { username, password } = req.body;
+
+  const encryptedPassword = await bcrypt.hash(password, 10);
+
+  try {
+    await User.create({
+      username,
+      password: encryptedPassword,
+    });
+
+    res.json({ message: "Success" });
+  } catch (error) {
+    res.json({ message: error });
+  }
 });
 
 app.listen(9999);
