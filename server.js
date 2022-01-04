6c89dec79c260ac2281bd0d8916a94f8af110432
Hritick Jaiswal <newbiehritick@gmail.com>
2022-01-04 21:43:28 +0530

    Added body-parser and fetch request from client side

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const path = require("path");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use("/", express.static(path.join(__dirname, "static")));
+app.use(bodyParser.json());
+
+app.post("/api/register", (req, res) => {
+  console.log(req.body);
+});
 
 app.listen(9999);
